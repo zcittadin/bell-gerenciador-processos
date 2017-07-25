6e88c02731135989b94b8b8f5d59b11fedc2e6a1
zcittadin <z_cittadin@hotmail.com>
2017-07-25 08:10:07 -0300

    Correção de import.

--- src/main/java/com/servicos/estatica/belluno/report/template/ProcessoReportTemplate.java
+++ src/main/java/com/servicos/estatica/belluno/report/template/ProcessoReportTemplate.java
@@ -12,7 +12,6 @@ import java.awt.Color;
 import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.Calendar;
 import java.util.Date;
 import java.util.Locale;
 
