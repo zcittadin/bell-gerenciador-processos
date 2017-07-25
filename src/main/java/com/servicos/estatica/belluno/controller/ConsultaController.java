8495052503c98c92ed022b9149ecbe025cbb1160
zcittadin <z_cittadin@hotmail.com>
2017-07-25 09:32:51 -0300

    Criação do layout da tela de consultas.

--- src/main/java/com/servicos/estatica/belluno/controller/ConsultaController.java
+++ src/main/java/com/servicos/estatica/belluno/controller/ConsultaController.java
@@ -5,10 +5,16 @@ import java.util.ResourceBundle;
 
 import com.servicos.estatica.belluno.app.ControlledScreen;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 
 public class ConsultaController implements Initializable, ControlledScreen {
 
+	@FXML
+	Rectangle recConsulta;
+	
 	ScreensController myController;
 
 	@Override
@@ -19,7 +25,7 @@ public class ConsultaController implements Initializable, ControlledScreen {
 
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
-		// TODO Auto-generated method stub
+		recConsulta.setFill(Color.TRANSPARENT);
 
 	}
 
